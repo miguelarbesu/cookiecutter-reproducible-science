ffba4dcedcc9052d4ed53f6046dc78802d0ff283
Miguel Arbesú <miguelarbesu@gmail.com>
2021-02-28 18:52:36 +0100

    fix: simplify github setup script
    dpkg check for gh CLI not working on default Arch

--- {{cookiecutter.project_slug}}/devtools/setup_github.sh
+++ {{cookiecutter.project_slug}}/devtools/setup_github.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-dpkg -s gh &> /dev/null  
-
-if [ $? -ne 0 ]
-
-    then
-        echo "GitHub CLI not installed"
-    else
-        echo "Creating GitHub repository..."
-        gh repo create {{ cookiecutter.project_slug}}
-        git push --set-upstream origin main
-fi
+printf "Creating GitHub repository..."
+gh repo create {{ cookiecutter.project_slug}}
+git push --set-upstream origin main
